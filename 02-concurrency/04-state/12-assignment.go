c9e9e08301c23cd1188dc52c4fd7f30cf2f2232c
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-17 16:08:50 +0530

    assignment - solution

--- 02-concurrency/04-state/12-assignment.go
+++ 02-concurrency/04-state/12-assignment.go
@@ -4,18 +4,36 @@ Refactor the program to find the prime numbers concurrently
 */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var primes []int
+var mutex sync.Mutex
 
 func main() {
-	var primes []int
+
 	start, end := 1000, 2000
+	wg := &sync.WaitGroup{}
 	for no := start; no <= end; no++ {
-		if isPrime(no) {
-			primes = append(primes, no)
-		}
+		wg.Add(1)
+		go processNo(wg, no)
 	}
+	wg.Wait()
 	fmt.Println("primes count :", len(primes))
 	fmt.Println(primes)
+}
+
+func processNo(wg *sync.WaitGroup, no int) {
+	defer wg.Done()
+	if isPrime(no) {
+		mutex.Lock()
+		{
+			primes = append(primes, no)
+		}
+		mutex.Unlock()
+	}
 }
 func isPrime(no int) bool {
 	for i := 2; i <= (no / 2); i++ {
