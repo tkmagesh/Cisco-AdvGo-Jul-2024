5be0a7fd32d85ba163cb9c319a1da82903a79f05
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-17 12:13:57 +0530

    updates

--- 01-Recap/06-panic-recovery-b.go
+++ 01-Recap/06-panic-recovery-b.go
@@ -5,9 +5,12 @@ connverting a panic into an error to allow course correction
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
+
+var ErrDivideByZero error = errors.New("divide by zero error")
 
 func main() {
 	defer func() {
@@ -23,8 +26,12 @@ func main() {
 		fmt.Scanln(&divisor)
 		// q, r := divide(100, divisor)
 		q, r, err := divideClient(100, divisor)
+		if err == ErrDivideByZero {
+			fmt.Println("divisor cannot be 0, try again!")
+			continue
+		}
 		if err != nil {
-			fmt.Println("Error occurred, error =", err)
+			fmt.Println("unknown error occurred, err =", err)
 			continue
 		}
 		fmt.Printf("Dividing 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
@@ -46,7 +53,12 @@ func divideClient(x, y int) (quotient, remainder int, err error) {
 
 // 3rd party api (cannot change)
 func divide(x, y int) (quotient, remainder int) {
+
 	fmt.Println("[divide] calculating quotient")
+	// raise a panic programmatically
+	if y == 0 {
+		panic(ErrDivideByZero)
+	}
 	quotient = x / y
 	fmt.Println("[divide] calculating remainder")
 	remainder = x % y
