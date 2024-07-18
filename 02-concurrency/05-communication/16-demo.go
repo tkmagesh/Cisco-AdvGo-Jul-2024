8344ed5e86a1757c8d3b046baea5d086b7ec14f4
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-18 10:09:56 +0530

    channel behaviors

--- 02-concurrency/05-communication/16-demo.go
+++ 02-concurrency/05-communication/16-demo.go
@@ -1,16 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// create a channel
 	ch := make(chan int)
 
-	// receive & print data from the channel
-	data := <-ch
-	fmt.Println(data)
+	/*
+		// "Send" operation in a goroutine
+		go func() {
+			// send the data to the channel
+			ch <- 100
+		}()
+
+		// receive & print data from the channel
+		data := <-ch
+		fmt.Println(data)
+	*/
+
+	/*
+		// "Recieve" operation in a goroutine
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func() {
+			// receive & print data from the channel
+			data := <-ch // (nb)
+			fmt.Println(data)
+			wg.Done()
+		}()
+
+		// send the data to the channel
+		ch <- 100 // (b)
+		wg.Wait()
+	*/
+
+	// "Recieve" operation in a goroutine (using a channel instead of a waitgroup)
+	doneCh := make(chan struct{})
+	go func() {
+		// receive & print data from the channel
+		data := <-ch // (nb)
+		fmt.Println(data)
+		doneCh <- struct{}{}
+	}()
 
 	// send the data to the channel
-	ch <- 100
-
+	ch <- 100 // (b)
+	<-doneCh
 }
