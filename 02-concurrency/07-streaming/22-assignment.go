87719d6a887fe973c9dea1c51c7284fc3b6d2acd
Magesh Kuppan <tkmagesh77@gmail.com>
2024-07-18 11:37:26 +0530

    assignment - solution

--- 02-concurrency/07-streaming/22-assignment.go
+++ 02-concurrency/07-streaming/22-assignment.go
@@ -2,3 +2,37 @@
 write a "genPrimes()" (producer) function that generates prime numbers within the given "start" and "end" asynchronously and returns the prime numbers one at a time as and when they are generated
 print the generated prime numbers in the main function (consumer) as and when they are generated
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	ch := genPrimes(1000, 2000)
+	for primeNo := range ch {
+		fmt.Println("primeNo :", primeNo)
+	}
+	fmt.Println("Done!")
+}
+
+func genPrimes(start, end int) chan int {
+	ch := make(chan int)
+	go func() {
+		for no := start; no <= end; no++ {
+			if isPrime(no) {
+				ch <- no
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
